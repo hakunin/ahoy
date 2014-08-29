0d41cfe558269c01291aeef4806bc76f690ef1be
Michal Hantl <michal.hantl@gmail.com>
2014-08-29 14:34:44 +0200

    Update warden.rb

--- lib/ahoy/warden.rb
+++ lib/ahoy/warden.rb
@@ -1,4 +1,5 @@
 Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
+  return if user.is_a? AdminUser
   request = ActionDispatch::Request.new(auth.env)
   ahoy = Ahoy::Tracker.new(request: request)
   ahoy.authenticate(user)
